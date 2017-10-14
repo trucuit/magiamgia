2e3640a29f2eb4e94394f9ed8165acf2432e27c5
Truc <15520939@gm.uit.edu.vn>
2017-10-14 13:02:06 +0700

    demo

---
+++ html/content.php
@@ -0,0 +1,12 @@
+<div id="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				<?php include_once 'html/content-col-8.html' ?>
+			</div>
+			<div class="col-4">
+				<?php include_once 'html/content-col-4.html' ?>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Ma Coupon</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/myCSS.css">
+	<link rel="stylesheet" href="css/css-header-8.css">
+	<link rel="stylesheet" href="css/css-header-4.css">
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<!-- header -->
+	<?php include_once 'html/header.html'; ?>
+	<!-- //header -->
+
+	<!-- content -->
+	<?php include_once 'html/content.php'; ?>
+	<!-- //content -->
+
+	<!-- footer -->
+	<?php include_once 'html/footer.html'; ?>
+	<!-- //footer -->
+</body>
+</html>

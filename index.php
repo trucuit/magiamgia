4637069b86ecc695f58171a89cae5b7f8fc1b806
huyhaonguyen <32436989+huyhaonguyen@users.noreply.github.com>
2017-10-18 16:24:15 +0700

    update2

--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ma Coupon</title>
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
 	<link rel="stylesheet" href="css/myCSS.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/css-header-8.css">
 	<link rel="stylesheet" href="css/css-header-4.css">
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&amp;subset=vietnamese" rel="stylesheet">
+
 
 </head>
 <body>
@@ -18,6 +21,10 @@
 	<!-- header -->
 	<?php include_once 'html/header.html'; ?>
 	<!-- //header -->
+
+	<!-- banner -->
+	<?php include_once 'html/banner.html' ?>
+	<!-- //banner -->
 
 	<!-- content -->
 	<?php include_once 'html/content.php'; ?>

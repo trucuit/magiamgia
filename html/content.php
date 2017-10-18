05dc702cd2c93d18671eb29da0cbe03070d5c4a3
Truc <15520939@gm.uit.edu.vn>
2017-10-18 08:13:08 +0700

    Merge branch 'master' of https://github.com/trucuit/magiamgia
    # Conflicts:
    #	css/css-header-4.css
    #	css/myCSS.css
    #	html/content-col-4.html
    #	html/content.php
    #	html/footer.html
    #	index.php

---
+++ html/content.php
@@ -0,0 +1,11 @@
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				<?php include_once 'html/content-col-8.html' ?>
+			</div>
+			<div class="col-md-4">
+				<?php include_once 'html/content-col-4.html' ?>
+			</div>
+		</div>
+	</div>

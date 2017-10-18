c8f53f904559c6ee748e60f3c71a4c020d0de901
Truc <15520939@gm.uit.edu.vn>
2017-10-18 08:48:52 +0700

    1

--- html/content.php
+++ html/content.php
@@ -1,4 +1,4 @@
-
+<div id="content">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8">
@@ -9,3 +9,4 @@
 			</div>
 		</div>
 	</div>
+</div>
--- index.php
+++ index.php
@@ -3,21 +3,24 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ma Coupon</title>
-	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/font-awesome.css">
 	<link rel="stylesheet" href="css/myCSS.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
 	<link rel="stylesheet" href="css/css-header-8.css">
 	<link rel="stylesheet" href="css/css-header-4.css">
 	<script src="js/jquery-3.2.1.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-
 </head>
 <body>
-<div class="wrap">
 	<!-- header -->
 	<?php include_once 'html/header.html'; ?>
 	<!-- //header -->
+
+	<!-- banner -->
+	<?php include_once 'html/banner.html' ?>
+	<!-- //banner -->
 
 	<!-- content -->
 	<?php include_once 'html/content.php'; ?>
@@ -26,6 +29,5 @@
 	<!-- footer -->
 	<?php include_once 'html/footer.html'; ?>
 	<!-- //footer -->
-</div>
 </body>
 </html>
